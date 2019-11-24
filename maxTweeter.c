22678b219773541201a188ce08122e25b44318d5
Matthew Ha <hello.matthewha@gmail.com>
2019-11-24 13:18:17 -0800

    find name idx

--- maxTweeter.c
+++ maxTweeter.c
@@ -10,33 +10,53 @@ typedef struct {
 
 char * TWEETER_COL_NAME = "name";
 
-Header * parse_header(char * line)
+Header * parse_header(char ** line)
 {
   Header * header = malloc(sizeof(Header));
   if (header == NULL) {
     return NULL;
   }
 
-  printf("name len: %i\n", strlen(TWEETER_COL_NAME));
-
+  header->tweeter_idx = -1;
 
   char * col = NULL;
   int i = 0;
+
   while( (col = strsep(&line, ",")) != NULL ) {
     printf("col entry: %s\n", col);
 
+    int len = strlen(col);
+    if (*col == '"') {
+      if (col[len-1] != '"') {
+        printf("Bad quote\n");
+        exit(1);
+      }
+
+      col[len-1] = '\0';
+      col = col + 1;
+      printf("col name: %s\n", col);
+    }
     
     int cmp = strcmp(col, TWEETER_COL_NAME);
-    printf("len: %i\n", strlen(col));
-
     if (cmp == 0) {
       printf("found name @ %i\n", i);
+      header->tweeter_idx = i;
+      break;
     }
+
+    col = col - 1;
+    col[len-1] = '"';
 
     i++;
   }
 
-  return NULL;
+  if (header->tweeter_idx == -1) {
+    printf("Name col not found\n");
+    exit(1);
+  }
+
+
+  return header;
 }
 
 void parse_row(char * line)
@@ -104,7 +124,7 @@ int main(int argc, const char* argv[]) {
 
   Header* header = parse_header(header_line);
 
-  printf("header cols: %s\n", header);
+  printf("header name idx: %i\n", header->tweeter_idx);
   
   printf("\n---- ENTRIES ----\n");
   // New line
