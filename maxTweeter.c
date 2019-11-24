240d87fe2ca5f2bc96ce6e8ec1104c6bccbf389e
Matthew Ha <hello.matthewha@gmail.com>
2019-11-24 12:13:38 -0800

    read file

--- maxTweeter.c
+++ maxTweeter.c
@@ -4,10 +4,40 @@
 
 
 int main(int argc, const char* argv[]) {
-  FILE *fp; 
+  FILE *filename; 
   if(argc < 2){
-    prrintf("No pathname specified\n"); 
+    printf("No pathname specified\n"); 
     return 1; 
+  }
+
+  filename = argv[1];
+
+  printf("filename: %s\n", filename);
+
+  char * buffer = 0;
+  long length;
+  FILE * file = fopen(filename, "rb");
+
+  if (file)
+  {
+    fseek(file, 0, SEEK_END);
+    length = ftell(file);
+
+    fseek(file, 0, SEEK_SET);
+    buffer = malloc(length);
+
+    if (buffer == NULL)
+    {
+      return 1;
+    }
+
+    fread(buffer, 1, length, file);
+    fclose (file);
+  }
+
+  if (buffer)
+  {
+    printf("contents: %s\n", buffer);
   }
 
   return 0;
