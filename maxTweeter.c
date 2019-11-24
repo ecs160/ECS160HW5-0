9022d54b9d765357a6c5b6054674d75fe5b0591e
Dhruv Nandakumar <dhruv.nandakumar@icloud.com>
2019-11-24 12:06:47 -0800

    Checks for path

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
+
 
 int main(int argc, const char* argv[]) {
-  printf("test");
+  FILE *fp; 
+  if(argc < 2){
+    prrintf("No pathname specified\n"); 
+    return 1; 
+  }
+
   return 0;
 }
