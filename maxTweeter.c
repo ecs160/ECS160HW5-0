87b1ad9e84cd340992f1def31fdf2d2c3e8d392b
Dhruv Nandakumar <dhruv.nandakumar@icloud.com>
2019-11-24 17:13:56 -0800

    Multiple name column fix

--- maxTweeter.c
+++ maxTweeter.c
@@ -103,23 +103,25 @@ void * parse_header(char ** line, int * ret_name_idx)
   int name_idx_match = -1;
 
   char * col_name = NULL;
+  int name_found = 0; 
   int curr_header_idx = 0;
 
   while( (col_name = strsep(&line, ",")) != NULL ) {
 
     int len = strlen(col_name);
-    if (col_name[0] == '"') {
-      if (col_name[len-1] != '"') {
-        throw_invalid_input();
-      }
+    
+    remove_quotes(&col_name, len);
 
-      remove_quotes(&col_name, len);
-    }
     
     if (strcmp(col_name, TWEETER_COL_NAME) == 0) {
       // printf("found name @ %i\n", i);
-      name_idx_match = curr_header_idx;
-      break;
+
+      if(name_found == 0){
+        name_idx_match = curr_header_idx;
+        name_found = 1; 
+      } else {
+        throw_invalid_input();
+      }
     }
 
     curr_header_idx++;
